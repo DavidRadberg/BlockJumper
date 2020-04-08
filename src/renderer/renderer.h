2ddf24bd5cb1ee6c78246f0a6b6f39e8a8c81164
David Rådberg <radberg.david@gmail.com>
2020-04-08 21:46:45 +0200

    Rendering triangle
    Added shaders and buffers to renderer

--- src/renderer/renderer.h
+++ src/renderer/renderer.h
@@ -1,6 +1,25 @@
 #pragma once
 
+#include <vector>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 class Renderer {
 public:
+    Renderer();
+
     void render();
+    bool check_shader(unsigned int shader);
+    bool check_program();
+
+private:
+    std::vector<float> vertices_;
+
+    unsigned int vbo_;
+    unsigned int vao_;
+
+    unsigned int vertex_shader_;
+    unsigned int fragment_shader_;
+    unsigned int shader_program_;
 };
