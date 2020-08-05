45cd977be5033dc746388dab401d374021a41656
David Rådberg <radberg.david@gmail.com>
2020-08-05 20:41:45 +0200

    Created jumping pose and cleanup in mario

--- src/objects/mario.cpp
+++ src/objects/mario.cpp
@@ -8,22 +8,22 @@ Mario::Mario(TEXTURES texture) : Mesh(MESHES::MARIO, texture) {
         float y = vertices_[3 * i + 1];
         if (x < 0.0) {
             if (y < 0.5) {
-                left_leg_indices_.push_back(i);
+                right_leg_indices_.push_back(i);
             } else if (x < -0.35 && y < 1.3) {
-                left_arm_indices_.push_back(i);
+                right_arm_indices_.push_back(i);
             }
         } else {
             if (y < 0.5) {
-                right_leg_indices_.push_back(i);
+                left_leg_indices_.push_back(i);
             } else if (x > 0.35 && y < 1.3) {
-                right_arm_indices_.push_back(i);
+                left_arm_indices_.push_back(i);
             }
         }
     }
 
     // set this rotation to org vert so we only have to do this once
-    rotate_points_z(left_arm_indices_, glm::vec3(-0.33, 1.05, 0.0),  1.35);
-    rotate_points_z(right_arm_indices_, glm::vec3(0.33, 1.05, 0.0), -1.35);
+    rotate_points_z(right_arm_indices_, glm::vec3(-0.33, 1.05, 0.0),  1.35);
+    rotate_points_z(left_arm_indices_, glm::vec3(0.33, 1.05, 0.0), -1.35);
     set_org_vertices();
 }
 
@@ -33,23 +33,44 @@ void Mario::animate(const CharacterState & state) {
     if (state.is_running()) {
         walk_state_ += state.speed * state.dt;
         animate_walk(walk_state_);
+    } else if (state.is_jumping() || state.is_falling()) {
+        // jumping pose
+        set_rotation_right_arm(-2.0);
+        set_rotation_left_arm(0.5);
+        set_rotation_right_leg(-0.5);
+        set_rotation_left_leg(0.25);
     } else {
         walk_state_ = 0.0;
     }
 }
 
+void Mario::set_rotation_right_arm(float angle) {
+    rotate_points_x(right_arm_indices_, glm::vec3(-0.33, 1.05, 0.0), angle);
+}
+
+void Mario::set_rotation_left_arm(float angle) {
+    rotate_points_x(left_arm_indices_, glm::vec3(0.33, 1.05, 0.0), angle);
+}
+
+void Mario::set_rotation_right_leg(float angle) {
+    rotate_points_x(right_leg_indices_, glm::vec3(0.0, 0.7, 0.0), angle);
+}
+
+void Mario::set_rotation_left_leg(float angle) {
+    rotate_points_x(left_leg_indices_, glm::vec3(0.0, 0.7, 0.0), angle);
+}
+
 void Mario::animate_walk(float s) {
-    float left_leg_rot =  -0.2 +  0.8 * glm::sin(25.0 * s);
-    float right_leg_rot = -0.2 + -0.8 * glm::sin(25.0 * s);
+    float right_leg_rot =  -0.2 +  0.8 * glm::sin(25.0 * s);
+    float left_leg_rot = -0.2 + -0.8 * glm::sin(25.0 * s);
 
-    float left_arm_rot =  -0.3 + -0.9 * glm::sin(25.0 * s + 0.5);
-    float right_arm_rot = -0.3 +  0.9 * glm::sin(25.0 * s + 0.5);
+    float right_arm_rot =  -0.3 + -0.9 * glm::sin(25.0 * s + 0.5);
+    float left_arm_rot = -0.3 +  0.9 * glm::sin(25.0 * s + 0.5);
 
-    rotate_points_x(left_leg_indices_, glm::vec3(0.0, 0.7, 0.0), left_leg_rot);
-    rotate_points_x(right_leg_indices_, glm::vec3(0.0, 0.7, 0.0), right_leg_rot);
-
-    rotate_points_x(left_arm_indices_, glm::vec3(-0.33, 1.05, 0.0), left_arm_rot);
-    rotate_points_x(right_arm_indices_, glm::vec3(0.33, 1.05, 0.0), right_arm_rot);
+    set_rotation_right_arm(right_arm_rot);
+    set_rotation_left_arm(left_arm_rot);
+    set_rotation_right_leg(right_leg_rot);
+    set_rotation_left_leg(left_leg_rot);
 
     rotate(0.02 * left_leg_rot);
 }
--- src/objects/mario.h
+++ src/objects/mario.h
@@ -15,5 +15,10 @@ private:
     std::vector<int> left_arm_indices_;
     std::vector<int> right_arm_indices_;
 
+    void set_rotation_right_arm(float angle);
+    void set_rotation_left_arm(float angle);
+    void set_rotation_right_leg(float angle);
+    void set_rotation_left_leg(float angle);
+
     float walk_state_ = 0.0;
 };
